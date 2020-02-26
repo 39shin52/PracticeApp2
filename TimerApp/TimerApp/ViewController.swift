c9cb057eca492b0ea3ef4a5731a4055244bff63f
shin52 <b1018088@fun.ac.jp>
2020-02-26 15:33:35 +0900

    first commit

---
+++ TimerApp/TimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Itou Shingo on 2020/02/26.
+//  Copyright © 2020 Itou Shingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
