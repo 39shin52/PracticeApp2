c723a8b1ec9a7b84444bb2e8d8b41d8c1eef4f7e
shin52 <b1018088@fun.ac.jp>
2020-03-10 14:32:02 +0900

    画面遷移やってみたけど落ちる

---
+++ TimerApp/TimerApp/ThminTimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  3minTimerViewController.swift
+//  TimerApp
+//
+//  Created by Itou Shingo on 2020/03/10.
+//  Copyright © 2020 Itou Shingo. All rights reserved.
+//
+
+import UIKit
+
+class 3minTimerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
